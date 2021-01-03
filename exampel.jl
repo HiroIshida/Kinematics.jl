2a6e34b277cac3668c97be8c4bcc6182eac29a00
HiroIshida <spitfire.docomo@gmail.com>
2021-01-03 21:26:08 +0900

    index access bench

--- exampel.jl
+++ exampel.jl
@@ -1,3 +1,18 @@
 using Kinematics
 urdf_path = Kinematics.__skrobot__.data.fetch_urdfpath()
-mech = parse_urdf(urdf_path)
+
+function main()
+    mech = parse_urdf(urdf_path)
+    torso_link = find_link(mech, "torso_lift_link")
+    function bench()
+        for i in 1:100000
+            for i in 1:20
+                parent_link(mech, torso_link)
+            end
+        end
+    end
+    @time bench()
+end
+
+main()
+
