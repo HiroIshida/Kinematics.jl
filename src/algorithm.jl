fcc2801ebd8e0d7fcf14711056336a49b9ffb54f
HiroIshida <spitfire.docomo@gmail.com>
2021-01-04 18:12:17 +0900

    set cache

--- src/algorithm.jl
+++ src/algorithm.jl
@@ -8,7 +8,12 @@ function _get_transform(m::Mechanism, hlink::Link)
     # plink : parent link
     @debugassert isempty(m.tf_stack)
     @debugassert isempty(m.link_id_stack)
+    tf_world_to_hlink = zero(Transform) # TODO with_base
     while(!isroot(hlink))
+        if iscached(m, hlink) 
+            tf_world_to_hlink = get_cache(m, hlink)
+            break
+        end
         plink = parent_link(m, hlink)
         hjoint = parent_joint(m, hlink)
         tf_plink_to_hjoint = hjoint.pose
@@ -19,15 +24,13 @@ function _get_transform(m::Mechanism, hlink::Link)
         push!(m.tf_stack, tf_plink_to_hlink)
         push!(m.link_id_stack, hlink.id)
         hlink = parent_link(m, hlink)
-        #set_cache!(m, hlink, tf_world_to_hlink)
     end
 
-    tf_world_to_hlink = zero(Transform) # TODO with_base
     while(!isempty(m.tf_stack))
-        # TODO link_id_stack will be used in set cache
-        here_id = pop!(m.link_id_stack)
+        hlink_id = pop!(m.link_id_stack)
         tf_plink_to_hlink = pop!(m.tf_stack)
         tf_world_to_hlink = tf_world_to_hlink * tf_plink_to_hlink
+        set_cache!(m, hlink_id, tf_world_to_hlink)
     end
     return tf_world_to_hlink
 end
