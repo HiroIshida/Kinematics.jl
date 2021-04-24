d672a9033798c08a51f8acf219832f1596f3ac6a
HiroIshida <spitfire.docomo@gmail.com>
2021-04-25 02:51:41 +0900

    Add test for planning

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using Kinematics
 using Test 
 using Rotations
+using StaticArrays
 import JSON
 Kinematics.debugging() = true
 
@@ -8,3 +9,4 @@ include("test_datastructure.jl")
 include("test_mechanism.jl")
 include("test_kinematics.jl")
 include("test_collision.jl")
+include("test_planning.jl")
