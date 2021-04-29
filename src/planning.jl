c4a54de00238e209f3d8c3f69809e3a77dcb638c
HiroIshida <spitfire.docomo@gmail.com>
2021-04-30 03:28:13 +0900

    Ipopt functions

--- src/planning.jl
+++ src/planning.jl
@@ -102,6 +102,73 @@ function nloptize(cons::Constraint)
     return inner
 end
 
+mutable struct IpoptManager
+    objective::Objective
+    ineqconst::IneqConst
+    eqconst::EqConst
+
+    jac_mat::Matrix{Float64}
+    idxes::Vector{Tuple{Int, Int}}
+
+    f_grad_cache::Vector{Float64}
+    g_jac_cache::Vector{Float64} # vector because jac is sparse matrix
+end
+
+function IpoptManager(objective::Objective, ineqconst::IneqConst, eqconst::EqConst) 
+    n_cons_total = eqconst.n_cons + ineqconst.n_cons
+    n_decision = eqconst.n_dof * eqconst.n_wp
+    jac_mat = ones(n_decision, n_cons_total) * Inf # actually, transponse of jac
+    
+    # adhoc
+    jac_mat_ineq = @view jac_mat[:, 1:ineqconst.n_cons]
+    jac_mat_eq = @view jac_mat[:, ineqconst.n_cons+1:end]
+    x_dummy = zeros(n_decision)
+    val_dummy_ineq = ones(ineqconst.n_cons)
+    val_dummy_eq = ones(eqconst.n_cons)
+
+    ineqconst(x_dummy, val_dummy_ineq, jac_mat_ineq)
+    eqconst(x_dummy, val_dummy_eq, jac_mat_eq)
+    idxes = Tuple{Int, Int}[]
+    for j in 1:size(jac_mat, 1)
+        for i in 1:size(jac_mat, 2)
+            if jac_mat[j, i] != Inf
+                push!(idxes, (i, j))
+            end
+        end
+    end
+
+    f_grad_cache = ones(n_decision) * Inf
+    g_jac_cache = ones(length(idxes)) * Inf
+    IpoptManager(objective, ineqconst, eqconst, jac_mat, idxes, f_grad_cache, g_jac_cache)
+end
+
+eval_f(ipoptm::IpoptManager, x) = ipoptm.objective(x, ipoptm.f_grad_cache)
+eval_f_grad(ipoptm::IpoptManager, x, grad) = (grad[:] = ipoptm.f_grad_cache)
+function eval_g(ipoptm::IpoptManager, x, val_vec)
+    n_cons_total = eqconst.n_cons + ineqconst.n_cons
+    n_decision = eqconst.n_dof * eqconst.n_wp
+    jac_mat_ineq = @view ipoptm.g_jac_cache[:, 1:ineqconst.n_cons]
+    jac_mat_eq = @view ipoptm.g_jac_cache[:, ineqconst.n_cons+1:end]
+    val_vec_ineq = @view val_vec[1:ineqconst.n_cons]
+    val_vec_eq = @view val_vec[ineqconst.n_cons+1:end]
+
+    ipoptm.ineqconst(x, val_vec_ineq, jac_mat_ineq)
+    ipoptm.eqconst(x, val_vec_eq, jac_mat_eq)
+end
+
+function eval_g_jac(ipoptm::IpoptManager, x, mode, rows, cols, values)
+    n_elems = length(ipoptm.idxes)
+    if mode==:Structure
+        for (i, pair) in zip(1:n_elems, ipoptm.idxes)
+            rows[i], cols[i] = pair 
+        end
+    else
+        for i in 1:n_elems
+            values[i] = ipoptm.g_jac_cache[pair[1], pair[2]]
+        end
+    end
+end
+
 function create_straight_trajectory(q_start, q_goal, n_wp)
     interval = (q_goal - q_start) / (n_wp - 1)
     xi = vcat([q_start + interval * (i-1) for i in 1:n_wp]...)
@@ -116,10 +183,10 @@ function construct_problem(
         )
     n_whole = n_dof * n_wp
 
-    f = Objective(n_wp, ones(n_dof))
+    F = Objective(n_wp, ones(n_dof))
     G = IneqConst(sscc, joints, sdf, n_wp)
     H = EqConst(n_dof, n_wp, q_start, q_goal)
-    return f, G, H, n_whole
+    return F, G, H, n_whole
 
 end
 
@@ -143,11 +210,12 @@ function plan_trajectory(
     @assert all(compute_coll_dists(sscc, joints, sdf) .> 0.0, dims=1)[1]
 
     xi_init = create_straight_trajectory(q_start, q_goal, n_wp)
-    f, G, H, n_whole = construct_problem(sscc, joints, sdf, q_start, q_goal, n_wp, n_dof)
+    F, G, H, n_whole = construct_problem(sscc, joints, sdf, q_start, q_goal, n_wp, n_dof)
+    man = IpoptManager(F, G, H)
 
     if solver==:NLOPT
         opt = Opt(:LD_SLSQP, n_whole)
-        opt.min_objective = (x::Vector, grad::Vector) -> f(x, grad)
+        opt.min_objective = (x::Vector, grad::Vector) -> F(x, grad)
         inequality_constraint!(opt, nloptize(G), [1e-8 for _ in 1:G.n_cons])
         equality_constraint!(opt, nloptize(H), [1e-8 for _ in 1:H.n_cons])
         opt.ftol_abs = ftol_abs
