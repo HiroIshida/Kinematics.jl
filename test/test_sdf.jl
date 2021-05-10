ceeeb31b69e08291f563ca06ad9f1d106290bd3b
HiroIshida <spitfire.docomo@gmail.com>
2021-05-11 06:54:57 +0900

    Fix test sdf for box

--- test/test_sdf.jl
+++ test/test_sdf.jl
@@ -1,12 +1,10 @@
 @testset "sdf" begin
     @testset "boxsdf" begin
-        pose = Transform(SVector3f(0.5, 0.5, 0.5))
+        pose = Transform(SVector3f(0.5, 0.5, 0.5), RotZ(0.3))
         width = SVector3f([1, 1, 1])
         box_sdf = BoxSDF(pose, width)
-        @test box_sdf(SVector3f([0., 0., 0])) == 0.0
-        @test box_sdf(SVector3f([1., 1., 1])) == 0.0
-        @test box_sdf(SVector3f([0.5, 0.5, 0.5])) == -0.5
-        @test box_sdf(SVector3f([2.0, 0., 0.])) == 1.0
-        @test box_sdf(SVector3f([-0.5, -0.5, -0.5])) ≈ 0.5 * sqrt(3)
+        @test box_sdf(pose * SVector3f([0.5, 0.5, 0.5])) ≈ 0.0
+        @test box_sdf(pose * SVector3f([0.0, 0.0, 0.0])) ≈ -0.5
+        @test box_sdf(pose * SVector3f([0.0, 0.0, 1.0])) ≈ 0.5
     end
 end
