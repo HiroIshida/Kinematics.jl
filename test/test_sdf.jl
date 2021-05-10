0f37c1d7b318a18fbd93cb12f6951b59d2f27c7a
HiroIshida <spitfire.docomo@gmail.com>
2021-05-11 08:06:30 +0900

    Add test for union sdf

--- test/test_sdf.jl
+++ test/test_sdf.jl
@@ -7,4 +7,17 @@
         @test box_sdf(pose * SVector3f([0.0, 0.0, 0.0])) ≈ -0.5
         @test box_sdf(pose * SVector3f([0.0, 0.0, 1.0])) ≈ 0.5
     end
+
+    @testset "unionsdf" begin
+        pose1 = Transform(SVector3f(0.5, 0.5, 0.0))
+        pose2 = Transform(SVector3f(-0.5, -0.5, 0.0))
+        width = SVector3f([1, 1, 1])
+        boxsdf1 = BoxSDF(pose1, width)
+        boxsdf2 = BoxSDF(pose2, width)
+        unionsdf = UnionSDF([boxsdf1, boxsdf2])
+        @test unionsdf(pose1 * SVector3f([0.5, 0.5, 0.5])) ≈ 0.0
+        @test unionsdf(pose2 * SVector3f([-0.5, -0.5, -0.5])) ≈ 0.0
+        @test unionsdf(pose1 * SVector3f([0.5, 0.5, 1.5])) ≈ 1.0
+        @test unionsdf(pose1 * SVector3f([-0.5, -0.5, -1.5])) ≈ 1.0
+    end
 end
