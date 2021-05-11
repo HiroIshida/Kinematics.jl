656b0a500c42b5207f12d5f6f0d6df4968081393
HiroIshida <spitfire.docomo@gmail.com>
2021-05-11 09:24:35 +0900

    Planning demo with fridge

--- fridge_demo.jl
+++ fridge_demo.jl
@@ -1,43 +1,50 @@
 using UUIDs
 using Revise
 using MeshCat
+using StaticArrays
 using Kinematics
+
+const SVector3f = SVector{3, Float64}
+
 urdf_path = "./data/complecated_fridge.urdf"
 fridge = parse_urdf(urdf_path, with_base=true)
 sdf = UnionSDF(fridge)
 
 robot = load_pr2(with_base=true)
-joints = rarm_joints(robot)
-collision_links = rarm_collision_links(robot)
+joints = vcat(rarm_joints(robot), larm_joints(robot))
+collision_links = vcat(larm_collision_links(robot), rarm_collision_links(robot))
 sscc = SweptSphereCollisionChecker(robot)
 for link in collision_links
     add_coll_links(sscc, link)
 end
 
 vis = Visualizer()
-#add_mechanism(vis, fridge)
+add_mechanism(vis, fridge)
 add_mechanism(vis, robot)
 
 joint = find_joint(fridge, "door_joint")
 reset_manip_pose(robot)
-set_joint_angles(fridge, [joint], [0.7, 1.5, 0.0, 0.0])
-#set_joint_angles(fridge, [joint], [0.0, 1.5, 0.0, 0.0])
+set_joint_angles(fridge, [joint], [2.0, 1.2, 0.0, 0.0])
+
+tf_fridge = get_transform(fridge, find_link(fridge, "base_link"))
+tf_target = Transform(SVector3f([0.0, 0.0, 1.2])) * tf_fridge
+add_frame(vis[:finger], tf_target)
+
+q_start = get_joint_angles(robot, joints)
+link = find_link(robot, "l_gripper_tool_frame")
+inverse_kinematics!(robot, link, joints, tf_target; with_rot=true) # pre solving
+q_goal = inverse_kinematics!(robot, link, joints, tf_target; with_rot=true, sscc=sscc, sdf=sdf)
+
+n_wp = 10
+margin = 0.03
+@time q_seq, status = plan_trajectory(sscc, joints, sdf, q_start, q_goal, n_wp, ftol_abs=1.0e-4, solver=:NLOPT, partial_consts=[], margin=margin)
 
 open(vis)
 update(vis, robot)
 update(vis, fridge)
 
-center = [1.5, 0., 0.]
-b_min = center + [-1., -1., 0]
-b_max = center + [+1., +1., 2.]
-w = b_max - b_min
-verts = [Kinematics.SVector3f(b_min + w .* rand(3)) for _ in 1:1000000]
-vals = [sdf(v) for v in verts]
-verts_extracted = verts[vals .< 0.0]
-println("computed")
-
-colors = [Kinematics.RGB(1., 0., 0.0) for _ in verts_extracted]
-setobject!(vis, PointCloud(verts_extracted, colors))
-
-tf = get_transform(fridge, sdf.attach_style.link)
-translation(tf)
+for i in 1:n_wp
+    sleep(1.0)
+    set_joint_angles(robot, joints, q_seq[:, i])
+    update(vis, robot)
+end
